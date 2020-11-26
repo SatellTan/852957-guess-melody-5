338d55697f3f39042b77468dca8952c66d88da21
Tatyana <taniaks2003@mail.ru>
2020-11-26 19:56:35 +0500

    Рефакторим действия: переходим от объекта, который содержит все действия, к отельным функциям. Каждая функция содержит одно конкретное действие.

--- src/store/action.js
+++ src/store/action.js
@@ -7,7 +7,36 @@ export const ActionType = {
   RESET_GAME: `RESET_GAME`,
 };
 
-export const ActionCreator = {
+export const incrementStep = () => ({
+  type: ActionType.INCREMENT_STEP,
+  payload: 1,
+});
+
+export const resetGame = () => ({
+  type: ActionType.RESET_GAME,
+});
+
+export const incrementMistake = (question, userAnswer) => {
+  let answerIsCorrect = false;
+
+  switch (question.type) {
+    case GameType.ARTIST:
+      answerIsCorrect = isArtistAnswerCorrect(question, userAnswer);
+      break;
+    case GameType.GENRE:
+      answerIsCorrect = isGenreAnswerCorrect(question, userAnswer);
+      break;
+  }
+
+  return {
+    type: ActionType.INCREMENT_MISTAKES,
+    payload: answerIsCorrect ? 0 : 1,
+  };
+};
+
+
+
+/*export const ActionCreator = {
   incrementStep: () => ({
     type: ActionType.INCREMENT_STEP,
     payload: 1,
@@ -32,4 +61,4 @@ export const ActionCreator = {
       payload: answerIsCorrect ? 0 : 1,
     };
   },
-};
+};*/
