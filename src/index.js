9b944a3b05b860da8b36063080159814b403ee0d
Tatyana <taniaks2003@mail.ru>
2020-12-01 19:39:12 +0500

    Добавлен редьюсер user. В модуле api-actions добавлены методы получения и отправки информации по авторизации.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import {createAPI} from "./services/api";
 import App from "./components/app/app";
 import rootReducer from "./store/reducers/root-reducer";
 import {requireAuthorization} from "./store/action";
-import {fetchQuestionList} from "./store/api-actions";
+import {fetchQuestionList, checkAuth} from "./store/api-actions";
 import {AuthorizationStatus} from "./const";
 
 const api = createAPI(
@@ -23,6 +23,7 @@ const store = createStore(
 );
 
 store.dispatch(fetchQuestionList());
+store.dispatch(checkAuth());
 
 ReactDOM.render(
     <Provider store={store}>
